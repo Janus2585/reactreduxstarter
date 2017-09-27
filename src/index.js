0f81a370f3a2aa65b0008fb08ef60398ed1d0498
Janus2585 <bd.yhome@gmail.com>
2017-09-27 02:01:53 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+//reactJS has split into 2 libraries: react and react-dom
+//to render something to the DOM, we need react-dom library.
+import ReactDOM from 'react-dom';
+
+
+
+//Create a new component. This component should produce
+//some HTML
+
+const App = () => {
+	return <div>Hi!</div>;
+}
+
+
+
+//Insert this component into the DOM 
+//App is a class. To render App to the DOM, we need to make an instance of the App class. To do this, we enclose App with a < /> tag
+//the second argument in render() is a target location to render at. Otherwise, it does not know where to render the component and throws an error
+ReactDOM.render(<App />, document.querySelector('.container')); 
